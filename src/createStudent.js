46994e4ba64cca55d6596f77fcf64e08eaedc2d7
marcelli-lima <marcelli_lima@hotmail.com>
2022-07-04 14:40:15 -0300

    requisito 5 feito

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,12 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
+const createStudent = (nome) => {
+let estudante = {
+  name: nome,
+  feedback: () => 'Eita pessoa boa!',
+};
+return estudante;
+};
 
 module.exports = createStudent;
